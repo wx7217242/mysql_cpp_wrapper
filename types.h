ec0cb72ba160794fc304f37bfa61ca489732fcbd
云微 <ilyatsatt@sina.com>
2017-06-02 17:53:09 +0800

    类型完善

--- types.h
+++ types.h
@@ -1,8 +1,18 @@
 #ifndef MYSQL_CPP_CONN_TYPES_H_
 #define MYSQL_CPP_CONN_TYPES_H_
 
+#ifdef WIN32
+typedef char			int8_t;
+typedef short			int16_t;
+typedef int				int32_t;
+typedef	long long		int64_t;
+typedef unsigned char	uint8_t;
+typedef unsigned short	uint16_t;
+typedef unsigned int	uint32_t;
+typedef	unsigned long long	uint64_t;
+typedef int				socklen_t;
+#else
 #include <stdint.h>
-
-
+#endif
 
 #endif
